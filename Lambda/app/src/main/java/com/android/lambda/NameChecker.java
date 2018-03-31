7eb0b5398804a6bb32bdadead6f0ad0072e9c2d4
xwpeng <l>
2018-03-31 10:27:39 +0800

    将Android的旧实验代码移除

--- Lambda/app/src/main/java/com/android/lambda/NameChecker.java
+++
@@ -1,9 +0,0 @@
-package com.android.lambda;
-
-/**
- * Created by Administrator on 2018/3/17.
- */
-
-public interface NameChecker {
-    boolean check(String name);
-}
--- Lambda/app/src/main/java/com/android/lambda/Shower.java
+++
@@ -1,9 +0,0 @@
-package com.android.lambda;
-
-/**
- * Created by Administrator on 2018/3/17.
- */
-
-public interface Shower {
-    void show(Person p);
-}
